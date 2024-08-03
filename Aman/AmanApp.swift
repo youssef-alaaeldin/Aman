dd16f5134698157e1420c344f254e1d9404e91e0
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2024-08-03 08:32:42 +0300

    Initial Commit

---
+++ Aman/AmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmanApp.swift
+//  Aman
+//
+//  Created by Yousuf Abdelfattah on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
