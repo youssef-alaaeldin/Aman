8e3895cb1de23568bc1094a3838c09e2bda215b8
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2024-08-10 14:20:56 +0300

    property details view

---
+++ Aman/View/Components/BuildingView.swift
@@ -0,0 +1,48 @@
+//
+//  DetailsRectangleHelper.swift
+//  Aman
+//
+//  Created by Yousuf Abdelfattah on 10/08/2024.
+//
+
+import SwiftUI
+
+struct BuildingView: View {
+    let property : Property
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Type")
+                    .foregroundStyle(Colors.Neutrals.n900)
+                Spacer()
+                Text(property.type.rawValue)
+                    .foregroundStyle(Colors.Neutrals.n600)
+            }
+            Divider()
+                .foregroundStyle(Colors.Neutrals.n200)
+            HStack {
+                Text("Year Built")
+                    .foregroundStyle(Colors.Neutrals.n900)
+                Spacer()
+                Text("2017")
+                    .foregroundStyle(Colors.Neutrals.n600)
+                
+            }
+            Divider()
+                .foregroundStyle(Colors.Neutrals.n200)
+            HStack {
+                Text("Living area")
+                    .foregroundStyle(Colors.Neutrals.n900)
+                Spacer()
+                Text("\(property.size.formatted())")
+                    .foregroundStyle(Colors.Neutrals.n600)
+            }
+        }
+        .padding()
+        .background(Colors.Neutrals.n100)
+        .cornerRadius(12)
+    }
+}
+#Preview {
+    BuildingView(property: Property(id: UUID(), name: "Youssef alaa", type: .Apartment, price: 553221, location: "Tagmo3", numberOfBedrooms: 3, numberOfBathrooms: 1, size: 255, imageURL: "2"))
+}
---
+++ Aman/View/Components/FeatureView.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureView.swift
+//  Aman
+//
+//  Created by Yousuf Abdelfattah on 10/08/2024.
+//
+
+import SwiftUI
+
+struct FeatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureView()
+}
